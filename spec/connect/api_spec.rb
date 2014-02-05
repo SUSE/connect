fb868bd295cdf3cbf1c7da35c99662be5f93b361
KalabiYau <skullzeek@gmail.com>
2014-02-05 15:09:50 +0100

    Api spec linted

--- spec/connect/api_spec.rb
+++ spec/connect/api_spec.rb
@@ -7,14 +7,14 @@ describe SUSE::Connect::Api do
   let :client do
     client = double('client')
     client.stub(:url => 'https://example.com')
-    client.stub(:options => {:token => 'token-shmocken'})
+    client.stub(:options => { :token => 'token-shmocken' })
     client
   end
 
   describe '.new' do
 
     it 'require client object' do
-      expect { subject.new() }.to raise_error ArgumentError
+      expect { subject.new }.to raise_error ArgumentError
     end
 
     it 'require client object' do
@@ -46,13 +46,24 @@ describe SUSE::Connect::Api do
     end
 
     it 'sends a call with basic auth and params to api' do
-      product = {:name => 'SLES', :version => '11-SP2', :arch => 'x86_64', :token => 'token-shmocken'}
-      expected_payload = { :product_ident => 'SLES', :product_version => '11-SP2', :arch => 'x86_64', :token => 'token-shmocken' }
-      SUSE::Connect::Connection.any_instance.should_receive(:post).with('/connect/systems/products', {:auth => 'basic_auth_mock', :params => expected_payload}).and_call_original
+      product = {
+          :name    => 'SLES',
+          :version => '11-SP2',
+          :arch    => 'x86_64',
+          :token   => 'token-shmocken'
+      }
+      expected_payload = {
+          :product_ident    => 'SLES',
+          :product_version  => '11-SP2',
+          :arch             => 'x86_64',
+          :token            => 'token-shmocken'
+      }
+      SUSE::Connect::Connection.any_instance.should_receive(:post)
+        .with('/connect/systems/products', :auth => 'basic_auth_mock', :params => expected_payload)
+        .and_call_original
       subject.new(client).activate_subscription('basic_auth_mock', product)
     end
 
   end
-
 
 end
--- spec/support/api_webmocks.rb
+++ spec/support/api_webmocks.rb
@@ -2,16 +2,21 @@ require 'json'
 
 def stub_announce_call
   response_body = JSON.parse(File.read('spec/fixtures/announce_response.json')).to_json
-  stub_request(:post, 'https://example.com/connect/subscriptions/systems').
-      with(:headers => {'Authorization'=>'token', 'Content-Type'=>'application/json'}).
-      to_return(:status => 200, :body => response_body, :headers => {})
+  stub_request(:post, 'https://example.com/connect/subscriptions/systems')
+    .with(:headers => { 'Authorization' => 'token', 'Content-Type' => 'application/json' })
+    .to_return(:status => 200, :body => response_body, :headers => {})
 end
 
 def stub_activate_call
   response_body = JSON.parse(File.read('spec/fixtures/activate_response.json')).to_json
   headers       = { 'Authorization' => 'basic_auth_mock', 'Content-Type' => 'application/json' }
-  request_body  = { :product_ident => 'SLES', :product_version => '11-SP2', :arch => 'x86_64',:token => 'token-shmocken' }
-  stub_request(:post, 'https://example.com/connect/systems/products').
-      with(:headers => headers, :body => request_body).
-      to_return(:status => 200, :body => response_body, :headers => {})
+  request_body  = {
+      :product_ident    => 'SLES',
+      :product_version  => '11-SP2',
+      :arch             => 'x86_64',
+      :token            => 'token-shmocken'
+  }
+  stub_request(:post, 'https://example.com/connect/systems/products')
+    .with(:headers => headers, :body => request_body)
+    .to_return(:status => 200, :body => response_body, :headers => {})
 end
