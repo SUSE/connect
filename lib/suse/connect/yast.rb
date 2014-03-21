6dda980275e94803c4a78739290d468cd77a2928
Will Stephenson <wstephenson@kde.org>
2014-03-21 15:02:25 +0100

    add existing functions to YaST module

--- lib/suse/connect/yast.rb
+++ lib/suse/connect/yast.rb
@@ -1,6 +1,19 @@
 module SUSE
   module Connect
     module YaST
+    end
+
+      def announce_system
+        response = @api.announce_system(Utilities::token_auth)
+        body = response.body
+        Zypper.write_base_credentials(body['login'], body['password'])
+      end
+
+      def activate_product(product)
+        response = @api.activate_subscription(Utilities::basic_auth, product)
+        service = Service.new(response.body['sources'], response.body['enabled'], response.body['norefresh'])
+        System.add_service(service)
+      end
 
   end
 end
