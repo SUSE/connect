d82deb5c41030ffd12dd0bd2ab7d147e80b7d624
Will Stephenson <wstephenson@kde.org>
2014-03-27 11:05:33 +0100

    Utilities is now a class

--- lib/suse/connect/client.rb
+++ lib/suse/connect/client.rb
@@ -26,14 +26,14 @@ module SUSE
       end
 
       def announce_system
-        response = @api.announce_system(Utilities::token_auth)
+        response = @api.announce_system(Utilities.token_auth(@options[:token]))
         body = response.body
         Zypper.write_base_credentials(body['login'], body['password'])
       end
 
       def activate_subscription
         base_product    = Zypper.base_product
-        response = @api.activate_subscription(Utilities::basic_auth, base_product)
+        response = @api.activate_subscription(Utilities.basic_auth, base_product)
         service = Service.new(response.body['sources'], response.body['enabled'], response.body['norefresh'])
         System.add_service(service)
       end
