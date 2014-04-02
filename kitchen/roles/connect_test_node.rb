6a29bac1ea1618d7c0b0706ae0d2ec6baa91733d
Vladislav Lewinn <vlewin@suse.de>
2014-04-02 13:22:09 +0200

    remove ohai recipe from role

--- kitchen/roles/connect_test_node.rb
+++ kitchen/roles/connect_test_node.rb
@@ -2,7 +2,6 @@ name 'connect_test_node'
 description 'Prepares VM instance for SUSEConnect testing'
 
 run_list(
-  'recipe[ohai]',
   'recipe[connect::packages]',
   'recipe[connect::rubygems]',
   'recipe[connect::suse_connect]'
