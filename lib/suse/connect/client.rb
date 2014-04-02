0eab2e01f127c3cac5a99e6528dd90a6764ad33e
Will Stephenson <wstephenson@kde.org>
2014-04-02 13:19:45 +0200

    Return auth methods from utilities

--- lib/suse/connect/client.rb
+++ lib/suse/connect/client.rb
@@ -20,22 +20,49 @@ module SUSE
         @api                = Api.new(self)
       end
 
+      def token_auth(token)
+        raise CannotBuildTokenAuth, 'token auth requested, but no token provided' unless token
+        "Token token=#{token}"
+      end
+
+      def basic_auth
+
+        username, password = System.credentials
+
+        if username && password
+          basic_encode(username, password)
+        else
+          raise CannotBuildBasicAuth, 'cannot get proper username and password'
+        end
+
+      end
+
+      # TODO find new name because this is specific to cli usage
       def execute!
-        announce_system unless System.registered?
-        activate_subscription
+        login, password = announce_system unless System.registered?
+        Zypper.write_base_credentials(login, password)
+
+        service = activate_subscription(Zypper.base_product)
+        System.add_service(service)
       end
 
       def announce_system
-        response = @api.announce_system(Utilities.token_auth(@options[:token]))
-        body = response.body
-        Zypper.write_base_credentials(body['login'], body['password'])
+        response = @api.announce_system(token_auth)
+        return response.body['login'], response.body['password']
       end
 
-      def activate_subscription
-        base_product    = Zypper.base_product
-        response = @api.activate_subscription(Utilities.basic_auth, base_product)
-        service = Service.new(response.body['sources'], response.body['enabled'], response.body['norefresh'])
-        System.add_service(service)
+      def activate_subscription(product)
+        response = @api.activate_subscription(basic_auth, product)
+        Service.new(response.body['sources'], response.body['enabled'], response.body['norefresh'])
+      end
+
+      # @param product [Hash] product to query extensions for
+      def products_for(product)
+        response = @api.addons(basic_auth, product)
+        extensions = response.each do |extension|
+          Extension.new(extension['name'], '', '', extension['zypper_name'])
+        end
+        extensions
       end
 
       private
