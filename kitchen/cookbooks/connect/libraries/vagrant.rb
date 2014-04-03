a2832824a3ed8e791f569768c2d5f84363c8b8ca
Vladislav Lewinn <vlewin@suse.de>
2014-04-03 11:10:02 +0200

    remove unused library

--- kitchen/cookbooks/connect/libraries/vagrant.rb
+++
@@ -1,9 +0,0 @@
-class Chef::Recipe
-  module Scc
-    class Checker
-      def self.deployed?(project_name)
-        ::File.exists?("/var/www/#{project_name}/current")
-      end
-    end
-  end
-end
