21e4a64d6860a1505171db312dbad5f0f5ce6bcf
Dominik Bamberger <bamboo@suse.com>
2014-04-03 15:08:25 +0200

    add SUSE::Connect::YaST::announce_system(params = {})

--- lib/suse/connect/yast.rb
+++ lib/suse/connect/yast.rb
@@ -1,3 +1,28 @@
 # YaST class provides methods emulating SCC's API.
 class SUSE::Connect::YaST
+
+  class << self
+
+    attr_accessor :options
+
+    # Announces the system to SCC (usually by taking a regcode).
+    # Writes SCC / system credentials to the system and
+    # additionally returns them for convenience.
+    #
+    # @param params [Hash] optional parameters:
+    #  - regcode [String]
+    #  - hostname [String]
+    #  - email [String]
+    #  - parent [String]
+    #  - hwinfo [Hash]
+    # == Returns:
+    # SCC / system credentials [Hash]:
+    #  - login [String]
+    #  - password [String]
+    def announce_system(params = {})
+      Client.new(params).announce_system
+    end
+
+  end
+
 end
---
+++ spec/connect/yast_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe SUSE::Connect::YaST do
+
+  subject { SUSE::Connect::YaST }
+
+  let(:options) { { regcode: 'regcode', email: 'foo@bar.zer' } }
+  let(:client) { mock 'client' }
+
+  describe '#announce_system' do
+
+    before do
+      Client.stub(:new).and_return(client)
+      client.stub :announce_system
+    end
+
+    it 'calls announce_system on an instance of Client' do
+      client.should_receive :announce_system
+      subject.announce_system
+    end
+
+    it 'forwards all params to Client.announce_system' do
+      Client.should_receive(:new).with(options).and_return(client)
+      subject.announce_system(options)
+    end
+
+    it 'falls to use an empty hash as params if none are specified' do
+      Client.should_receive(:new).with({}).and_return(client)
+      subject.announce_system
+    end
+
+  end
+
+end
