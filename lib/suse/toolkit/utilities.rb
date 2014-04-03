0fe54061548ff7d4f72cced165a02f97210e4a52
KalabiYau <skullzeek@gmail.com>
2014-04-03 16:34:57 +0200

    Point to proper classes

--- lib/suse/toolkit/utilities.rb
+++ lib/suse/toolkit/utilities.rb
@@ -6,18 +6,18 @@ module SUSE
       include ::Net::HTTPHeader
 
       def token_auth(token)
-        raise CannotBuildTokenAuth, 'token auth requested, but no token provided' unless token
+        raise SUSE::Connect::CannotBuildTokenAuth, 'token auth requested, but no token provided' unless token
         "Token token=#{token}"
       end
 
       def basic_auth
 
-        username, password = System.credentials
+        username, password = SUSE::Connect::System.credentials
 
         if username && password
           basic_encode(username, password)
         else
-          raise CannotBuildBasicAuth, 'cannot get proper username and password'
+          raise SUSE::Connect::CannotBuildBasicAuth, 'cannot get proper username and password'
         end
 
       end
