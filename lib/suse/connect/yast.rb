538b4343641033bcce04655bccda4a626eedb772
Dominik Bamberger <bamboo@suse.com>
2014-04-03 15:43:19 +0200

    add SUSE::Connect::YaST.activate_product

--- lib/suse/connect/yast.rb
+++ lib/suse/connect/yast.rb
@@ -5,7 +5,8 @@ class SUSE::Connect::YaST
 
     attr_accessor :options
 
-    # Announces the system to SCC (usually by taking a token / regcode).
+    # Announces the system to SCC / the registration server.
+    # Usually expects a token / regcode to identify the correct subscription.
     # Writes SCC / system credentials to the system and
     # additionally returns them for convenience.
     #
@@ -21,6 +22,21 @@ class SUSE::Connect::YaST
     #  - password [String]
     def announce_system(params = {})
       Client.new(params).announce_system
+    end
+
+    # Activates a product on SCC / the registration server.
+    # Expects a product_ident to identify the correct service.
+    # Mostly requires token / regcode (except for free extensions or upgrades).
+    # Returns a service for the activated product
+    #
+    # @param params [Hash] optional parameters:
+    #  - token [String]
+    #  - product_ident [String]
+    #  - product_version [String]
+    #  - arch [String]
+    #  - email [String]
+    def activate_product(params = {})
+      Client.new(params).activate_product(params[:product_ident])
     end
 
   end
--- spec/connect/yast_spec.rb
+++ spec/connect/yast_spec.rb
@@ -4,30 +4,61 @@ describe SUSE::Connect::YaST do
 
   subject { SUSE::Connect::YaST }
 
-  let(:options) { { token: 'regcode', email: 'foo@bar.zer' } }
-  let(:client) { mock 'client' }
+  let(:params) { { token: 'regcode', email: 'foo@bar.zer' } }
+  let(:client) { double 'client' }
+
+  before { Client.stub(:new).and_return(client) }
 
   describe '#announce_system' do
 
-    before do
-      Client.stub(:new).and_return(client)
-      client.stub :announce_system
-    end
+    before { client.stub :announce_system }
 
     it 'calls announce_system on an instance of Client' do
       client.should_receive :announce_system
       subject.announce_system
     end
 
-    it 'forwards all params to Client.announce_system' do
-      Client.should_receive(:new).with(options).and_return(client)
-      subject.announce_system(options)
+    it 'forwards all params to an instance of Client' do
+      Client.should_receive(:new).with(params).and_return(client)
+      subject.announce_system params
     end
 
-    it 'falls to use an empty hash as params if none are specified' do
+    it 'falls back to use an empty Hash as params if none are specified' do
       Client.should_receive(:new).with({}).and_return(client)
       subject.announce_system
     end
+
+  end
+
+  describe '#activate_product' do
+
+    before { client.stub :activate_product }
+
+    it 'calls activate_product on an instance of Client' do
+      client.should_receive :activate_product
+      subject.activate_product
+    end
+
+    it 'forwards all params to an instance of Client' do
+      Client.should_receive(:new).with(params).and_return(client)
+      subject.activate_product params
+    end
+
+    it 'falls back to use an empty Hash as params if none are specified' do
+      Client.should_receive(:new).with({}).and_return(client)
+      subject.activate_product
+    end
+
+    it 'uses product_ident as parameter for Client#activate_product' do
+      params[:product_ident] = 'SLE95'
+      pp params
+      client.should_receive(:activate_product).with('SLE95')
+      subject.activate_product params
+    end
+
+  end
+
+  describe '#list_products' do
 
   end
 
