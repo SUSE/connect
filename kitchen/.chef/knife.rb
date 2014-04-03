2d3a7b90e7970aacff5c86ed1102e7e0e36d62b7
Vladislav Lewinn <vlewin@suse.de>
2014-04-03 10:47:12 +0200

    update chef/knife.rb config

--- kitchen/.chef/knife.rb
+++ kitchen/.chef/knife.rb
@@ -1,8 +1,6 @@
-cookbook_path    ["cookbooks", "site-cookbooks"]
+cookbook_path    ["cookbooks"]
 node_path        "nodes"
 role_path        "roles"
 environment_path "environments"
 data_bag_path    "data_bags"
-#encrypted_data_bag_secret "data_bag_key"
-
-knife[:berkshelf_path] = "cookbooks"
+knife[:berkshelf] = true
