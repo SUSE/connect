12abb67082e819892e6c2fc163a9a0a251c12902
Vladislav Lewinn <vlewin@suse.de>
2014-04-03 09:57:10 +0200

    instal osc package manually

--- kitchen/cookbooks/connect/recipes/packages.rb
+++ kitchen/cookbooks/connect/recipes/packages.rb
@@ -1,3 +1,13 @@
+remote_file "/tmp/osc-0.144.1-130.1.x86_64.rpm" do
+  source "http://download.opensuse.org/repositories/openSUSE:/Tools/SLE_11_SP3/x86_64/osc-0.144.1-130.1.x86_64.rpm"
+  mode 0644
+  action :create_if_missing
+end
+
+execute 'install osc package' do
+  command 'zypper --non-interactive --no-gpg-checks --quiet install --auto-agree-with-licenses /tmp/osc-0.144.1-130.1.x86_64.rpm'
+end
+
 node[:connect][:packages].each do |name, install|
   if install
     package name do
