ee659b33b89f3c023b8f575718bdd9c18f14885a
Vladislav Lewinn <vlewin@suse.de>
2014-04-04 08:55:03 +0200

    osc package is available again in openSUSE/Tools repo

--- kitchen/cookbooks/connect/recipes/packages.rb
+++ kitchen/cookbooks/connect/recipes/packages.rb
@@ -1,15 +1,15 @@
-package_url = 'http://download.opensuse.org/repositories/devel:/tools:/scm/SLE_11_SP3/x86_64/'
-package_name = 'osc-0.145.git.1396510341-84.1.x86_64.rpm'
+package_url = 'http://download.opensuse.org/repositories/openSUSE:/Tools/SLE_11_SP3/x86_64/'
+package_name = 'osc-0.145.0-131.1.x86_64.rpm'
 
-remote_file '/tmp/osc-0.144.1-130.1.x86_64.rpm' do
-  source "#{package_url}#{package_name}"
+remote_file "/tmp/#{package_name}" do
+  source package_url
   mode 0644
   action :create_if_missing
 end
 
 zypper_cmd = 'zypper --non-interactive --no-gpg-checks --quiet install --auto-agree-with-licenses'
 execute 'install osc package' do
-  command "#{zypper_cmd} /tmp/osc-0.144.1-130.1.x86_64.rpm"
+  command "#{zypper_cmd} /tmp/#{package_name}"
 end
 
 node[:connect][:packages].each do |name, install|
--- kitchen/cookbooks/connect/spec/packages_spec.rb
+++ kitchen/cookbooks/connect/spec/packages_spec.rb
@@ -13,6 +13,10 @@ describe 'connect::packages' do
     runner.converge('connect::packages')
   end
 
+  let(:osc_package) do
+    'osc-0.145.0-131.1.x86_64.rpm'
+  end
+
   it 'should install all required packages' do
     expect(chef_run).to install_package('gcc')
     expect(chef_run).to install_package('ruby-devel')
@@ -24,14 +28,13 @@ describe 'connect::packages' do
   end
 
   it 'downloads osc package' do
-    expect(chef_run).to create_remote_file_if_missing('/tmp/osc-0.144.1-130.1.x86_64.rpm')
+    expect(chef_run).to create_remote_file_if_missing("/tmp/#{osc_package}")
   end
 
   it 'installs osc package' do
     zypp_cmd = 'zypper --non-interactive --no-gpg-checks --quiet install --auto-agree-with-licenses'
-    package_src = '/tmp/osc-0.144.1-130.1.x86_64.rpm'
     expect(chef_run).to run_execute('install osc package').with(
-      command: "#{zypp_cmd} #{package_src}"
+      command: "#{zypp_cmd} /tmp/#{osc_package}"
     )
   end
 end
