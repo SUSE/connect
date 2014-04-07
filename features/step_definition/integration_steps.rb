a0806940d73d266f5a115c9d78eea25750b90447
Vladislav Lewinn <vlewin@suse.de>
2014-04-07 14:25:21 +0200

    basic system anounce test(WIP)

---
+++ features/step_definition/integration_steps.rb
@@ -0,0 +1,14 @@
+Given(/^I register a system with regcode '(.+)'$/) do |regcode|
+  step "I run `SUSEConnect --token #{regcode}`"
+  step 'the exit status should be 0'
+end
+
+Then(/^SUSEConnect should create the '(.+)' file$/) do |file|
+  step 'a file named "/etc/zypp/credentials.d/SCCcredentials" should exist'
+end
+
+And(/^Credentials file should contain 'SCC' prefixed system guid$/) do
+  step "I run `cat /etc/zypp/credentials.d/SCCcredentials`"
+  step 'the stdout should contain "username=SCC_"'
+end
+
