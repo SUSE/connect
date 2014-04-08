11fb4ed43a74b8e8af6c020bc385b1d16f35f2cc
Vladislav Lewinn <vlewin@suse.de>
2014-04-08 09:48:34 +0200

    clean up and add more integration steps

--- features/step_definition/integration_steps.rb
+++ features/step_definition/integration_steps.rb
@@ -1,24 +1,35 @@
-Given(/^I register a system with regcode '(.+)'$/) do |regcode|
-  step "I run `SUSEConnect --token #{regcode}`"
-  step 'the exit status should be 0'
-end
-
 Given(/^I register a system with (valid|invalid) regcode$/) do |condition|
   if condition == 'valid'
-    puts ENV['REGCODE'].inspect
-    step "I run `SUSEConnect --token #{ENV['REGCODE']}`"
+    step "I run `SUSEConnect -r #{ENV['REGCODE']} #{"--url http://localhost:3000" if ENV['LOCAL_SERVER']}`"
   else
-    step 'I run `SUSEConnect --token INVALID_REGCODE`'
+    step 'I run `SUSEConnect -r INVALID_REGCODE`'
   end
 
   step 'the exit status should be 0'
 end
 
-Then(/^SUSEConnect should create the '(.+)' file$/) do |file|
-  step 'a file named "/etc/zypp/credentials.d/SCCcredentials" should exist'
+Then(/^SUSEConnect should create the '(.+)' file$/) do |file_name|
+  file_name = file_name.include?('SCCcredentials') ? file_name : service_name
+  file = "/etc/zypp/credentials.d/#{file_name}"
+
+  step "a file named \"#{file}\" should exist"
 end
 
-And(/^Credentials file should contain 'SCC' prefixed system guid$/) do
-  step 'I run `cat /etc/zypp/credentials.d/SCCcredentials`'
-  step 'the stdout should contain "username=SCC_"'
+And(/^(Service credentials|SCCcredentials) file should contain '(.+)' prefixed system guid$/) do |file_name, prefix|
+  file_name = file_name.include?('SCCcredentials') ? file_name : service_name
+  file = "/etc/zypp/credentials.d/#{file_name}"
+
+  step "the file \"#{file}\" should contain \"#{prefix}\""
+end
+
+Then(/^SUSEConnect should add a new zypper service for base product$/) do
+  step 'I run `zypper ls`'
+  step "the output should contain \"#{service_name}\""
+  step 'the exit status should be 0'
+end
+
+Then(/^SUSEConnect should add a new repositories for base product$/) do
+  step 'I run `zypper lr`'
+  step "the output should contain \"#{service_name}\""
+  step 'the exit status should be 0'
 end
