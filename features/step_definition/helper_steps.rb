47824553c3b9980401f9d6c42f275571f264ef56
Vladislav Lewinn <vlewin@suse.de>
2014-04-08 10:14:33 +0200

    on SLES base_product[:name] is a short product name e.g. 'SLES' but the service name is based on product friendly name

--- features/step_definition/helper_steps.rb
+++ features/step_definition/helper_steps.rb
@@ -1,6 +1,10 @@
 def service_name
   base_product = SUSE::Connect::Zypper.base_product
-  @service_name ||= "#{base_product[:name]}_#{base_product[:version]}_#{base_product[:arch]}"
+  if base_product[:name] == 'openSUSE'
+    @service_name ||= "#{base_product[:name]}_#{base_product[:version]}_#{base_product[:arch]}"
+  else
+    @service_name ||= "#{base_product[:summary].gsub(' ', '_')}_#{base_product[:arch]}"
+  end
 end
 
 And(/^I wait a while$/) do
