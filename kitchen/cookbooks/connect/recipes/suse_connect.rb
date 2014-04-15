fec8c6c9290c6e2403da27697945bb77fd985ea9
Vladislav Lewinn <vlewin@suse.de>
2014-04-15 09:03:19 +0200

    skip certificate validation check

--- kitchen/cookbooks/connect/recipes/suse_connect.rb
+++ kitchen/cookbooks/connect/recipes/suse_connect.rb
@@ -50,7 +50,7 @@ osc_url = 'https://api.suse.de'
 osc_build = "osc -A #{osc_url} build #{node[:connect][:osc][:project]} #{node[:connect][:osc][:arch]} --no-verify"
 
 execute 'build SUSEConnect RPM' do
-  command "export PYTHONPATH=#{python_path}; #{osc_build}"
+  command "export PYTHONPATH=#{python_path}; echo 2|#{osc_build}"
   cwd "#{node[:connect][:project]}/package"
 end
 
