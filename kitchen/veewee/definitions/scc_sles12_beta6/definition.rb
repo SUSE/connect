3e2d331046b2f5b07eb5ce31e4e90bb0bfa249e8
Vladislav Lewinn <vlewin@suse.de>
2014-05-26 11:30:02 +0200

    make rubocop happy

--- kitchen/veewee/definitions/scc_sles12_beta6/definition.rb
+++ kitchen/veewee/definitions/scc_sles12_beta6/definition.rb
@@ -1,4 +1,4 @@
-Veewee::Session.declare({
+Veewee::Session.declare(
   :os_type_id  => 'OpenSUSE_64',
   :cpu_count   => '1',
   :memory_size => '1024',
@@ -11,12 +11,12 @@ Veewee::Session.declare({
   :iso_download_timeout => '1000',
   :boot_wait         => '10',
   :boot_cmd_sequence => [
-      '<Esc><Enter>',
-      'linux netdevice=eth0 netsetup=dhcp net.ifnames=0 install=cd:/',
-      ' lang=en_US',
-      ' autoyast=http://%IP%:8888/autoinst.xml',
-      ' insecure=1 textmode=1',
-      '<Enter>'
+    '<Esc><Enter>',
+    'linux netdevice=eth0 netsetup=dhcp net.ifnames=0 install=cd:/',
+    ' lang=en_US',
+    ' autoyast=http://%IP%:8888/autoinst.xml',
+    ' insecure=1 textmode=1',
+    '<Enter>'
   ],
   :ssh_login_timeout => '10000',
   :ssh_user          => 'vagrant',
@@ -26,13 +26,13 @@ Veewee::Session.declare({
   :ssh_guest_port    => '222',
   :sudo_cmd     => "echo '%p'|sudo -S sh '%f'",
   :shutdown_cmd => 'halt',
-  :postinstall_files   => ["postinstall.sh"],
+  :postinstall_files   => ['postinstall.sh'],
   :postinstall_timeout => '10000',
   :hooks => {
-      :before_create => Proc.new do
-        puts "Assembly started at #{Time.now}"
-        path = "#{Dir.pwd}/definitions/#{definition.box.name}"
-        Thread.new { WEBrick::HTTPServer.new(:Port => 8888, :DocumentRoot => path).start }
-      end
+    :before_create => proc do
+      puts "Assembly started at #{Time.now}"
+      path = "#{Dir.pwd}/definitions/#{definition.box.name}"
+      Thread.new { WEBrick::HTTPServer.new(:Port => 8888, :DocumentRoot => path).start }
+    end
   }
-})
+)
