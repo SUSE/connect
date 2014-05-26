ac2910e382442f64bb64193b8e7bcd48b94a75d1
Vladislav Lewinn <vlewin@suse.de>
2014-05-26 11:24:17 +0200

    raise an error if regcode empty

--- features/step_definition/integration_steps.rb
+++ features/step_definition/integration_steps.rb
@@ -1,11 +1,9 @@
 Given(/^I register a system with (valid|invalid) regcode$/) do |condition|
   regcode = condition == 'valid' ? ENV['REGCODE'] : 'INVALID_REGCODE'
-
-  url = ENV['LOCAL_SERVER'] || 'https://barium.scc.suse.de --insecure'
-  connect_cmd = "SUSEConnect -r #{regcode} --url #{url}"
+  connect_cmd = "SUSEConnect -r #{regcode}"
   response = `#{connect_cmd}`
 
-  puts "API ERROR: #{response.inspect}" unless $?.exitstatus.zero? # rubocop:disable SpecialGlobalVars
+  raise "API ERROR: #{response.inspect}" unless $?.exitstatus.zero? # rubocop:disable SpecialGlobalVars
 end
 
 Then(/^SUSEConnect should create the '(.+)' file$/) do |file_name|
