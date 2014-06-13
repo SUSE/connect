4adebcee2705f285e420dcb727fd29235a71b186
Vladislav Lewin <vlewin@suse.de>
2014-06-13 11:52:10 +0200

    add ZypperError and SystemCallError

--- lib/suse/connect/errors.rb
+++ lib/suse/connect/errors.rb
@@ -6,6 +6,8 @@ module SUSE
     class CannotBuildTokenAuth < StandardError; end
     class TokenNotPresent < StandardError; end
     class CannotDetectBaseProduct < StandardError; end
+    class ZypperError < StandardError; end
+    class SystemCallError < StandardError; end
 
     # Basic error for API interactions. Collects HTTP response (which includes
     # status code and response body) for future showing to user via {Cli}
@@ -25,7 +27,6 @@ module SUSE
       def message
         @response.body['localized_error']
       end
-
     end
 
   end
