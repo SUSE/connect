01f8e675539437a639ee3a6c543cc0d96d70d5fb
Vladislav Lewin <vlewin@suse.de>
2014-06-13 11:53:03 +0200

    fix system tests

--- spec/toolkit/system_calls_spec.rb
+++ spec/toolkit/system_calls_spec.rb
@@ -3,34 +3,56 @@ require 'suse/toolkit/system_calls'
 
 describe SUSE::Toolkit::SystemCalls do
 
-  subject { Zypper }
+  subject { Open3 }
+  let(:date) { 'Fr 13. Jun 10:50:53 CEST 2014' }
+  let(:success) { double("Process Status", :exitstatus => 0)}
+  let(:failure) { double("Process Status", :exitstatus => 1)}
+  let(:error_message) { 'Root privileges are required for installing or uninstalling packages' }
+
+  include SUSE::Toolkit::SystemCalls
 
   before do
     subject.send(:include, SUSE::Toolkit::SystemCalls)
   end
 
-  describe '.call' do
+  describe '.execute' do
 
-    it 'should make a system call' do
-      subject.should_receive(:system).with('date').and_return(true)
-      subject.send(:call, 'date').should be true
+    it 'should make a quiet system call' do
+      subject.should_receive(:capture3).with('date').and_return([date, '', success])
+      self.execute('date').should be nil
     end
 
-    it 'should produce log output if call failed' do
-      subject.should_receive(:system).with('date').and_return(false)
+    it 'should produce debug log output' do
+      SUSE::Connect::GlobalLogger.instance.log.should_receive(:debug)
+      subject.should_receive(:capture3).with('date').and_return([date, '', success])
+      expect(self.execute('date', false)).to eql date
+    end
+
+    it 'should produce error log output' do
       SUSE::Connect::GlobalLogger.instance.log.should_receive(:error)
-      subject.send(:call, 'date')
+
+      subject.should_receive(:capture3).with('unknown').and_return(['', error_message, failure])
+      expect {self.execute('unknown')}.to raise_error(SUSE::Connect::SystemCallError, error_message)
     end
-
-  end
-
-  describe '.call_with_output' do
 
     it 'should make a system call and return output from system' do
-      subject.should_receive(:'`').with('date').and_return("Thu Mar 13 12:22:51 CET 2014\n")
-      subject.send(:call_with_output, 'date')
+      subject.should_receive(:capture3).with('date').and_return([date, '', success])
+      expect(self.execute('date', false)).to eql date
     end
 
+    it 'should raise SystemCallError exception' do
+      subject.should_receive(:capture3).with('unknown').and_return(['', error_message, failure])
+
+      SUSE::Connect::GlobalLogger.instance.log.should_receive(:error)
+      expect {self.execute('unknown')}.to raise_error(SUSE::Connect::SystemCallError, error_message)
+    end
+
+    it 'should raise ZypperError exception' do
+      subject.should_receive(:capture3).with('zypper unknown').and_return(['', error_message, failure])
+
+      SUSE::Connect::GlobalLogger.instance.log.should_receive(:error)
+      expect {self.execute('zypper unknown')}.to raise_error(SUSE::Connect::ZypperError, error_message)
+    end
   end
 
 end
