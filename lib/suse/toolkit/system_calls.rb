535483ebe616607f3d5f8ead49f25c149c5fb68f
Vladislav Lewin <vlewin@suse.de>
2014-06-13 11:51:09 +0200

    use open3 library for system calls

--- lib/suse/toolkit/system_calls.rb
+++ lib/suse/toolkit/system_calls.rb
@@ -1,17 +1,27 @@
+require 'open3'
+
 module SUSE
   module Toolkit
     # Provides basic system calls interface
     module SystemCalls
       include Connect::Logger
 
-      def call(command)
-        log.debug("Calling: '#{command}'")
-        system(command) ? true : log.error("command '#{command}' failed")
-      end
+      def execute(cmd, quiet=true)
+        log.debug("Executing: '#{cmd}' Quiet: #{quiet}")
 
-      def call_with_output(command)
-        log.debug("Calling: '#{command}'")
-        `#{command}`.chomp
+        output, error, status = Open3.capture3(cmd) {|stdin, stdout, stderr, wait_thr| stdout.read }
+
+        if status.exitstatus.zero?
+          output.strip unless quiet
+        else
+          log.error("command '#{cmd}' failed")
+
+          if cmd.include? 'zypper'
+            raise Connect::ZypperError, error
+          else
+            raise Connect::SystemCallError, error
+          end
+        end
       end
 
     end
