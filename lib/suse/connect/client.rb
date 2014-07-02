03cb8bf0a78b58d9731ac32f4203f027fd004b61
jschmid1 <jschmid@suse.de>
2014-07-02 13:08:30 +0200

    client module[changed order of messages]

--- lib/suse/connect/client.rb
+++ lib/suse/connect/client.rb
@@ -48,9 +48,10 @@ module SUSE
 
       def success_message
         puts "Registered #{@options[:product][:identifier]} #{@options[:product][:version]} #{@options[:product][:arch]}"
-        puts "Using EMail: #{@options[:email]}" unless  @options[:email].nil?
         puts "Rooted at: #{@options[:filesystem_root]}" unless @options[:filesystem_root].nil?
         puts "To server: #{@options[:url]}" unless @options[:url].nil?
+        puts "Using EMail: #{@options[:email]}" unless  @options[:email].nil?
+
       end
 
       # @returns: Empty body and 204 status code
