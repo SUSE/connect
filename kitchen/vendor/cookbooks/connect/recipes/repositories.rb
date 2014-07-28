5f6b2ea0c9f54b272f4f566cd5f42861d393f7ba
Will Stephenson <wstephenson@suse.de>
2014-07-28 13:55:35 +0200

    Use darix' temporary repo for new gem2rpm

--- kitchen/vendor/cookbooks/connect/recipes/repositories.rb
+++ kitchen/vendor/cookbooks/connect/recipes/repositories.rb
@@ -6,6 +6,10 @@ execute 'add_sle_12_repo' do
   command 'zypper --non-interactive ar http://download.suse.de/ibs/SUSE:/SLE-12:/GA/standard/ SLE-12-standard'
 end
 
+execute 'add_darix_gem2rpm_repo' do
+  command 'zypper --non-interactive ar http://download.suse.de/ibs/home:/darix:/ruby/SLE_12/ darix-gem2rpm'
+end
+
 execute 'add_osc_repo' do
   command 'zypper --non-interactive ar http://download.opensuse.org/repositories/openSUSE:/Tools/SLE_11_SP3/ OSC'
 end
