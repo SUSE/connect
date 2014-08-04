1fa1a8afcfd895bb9812cd917201ed9a8aeed3c0
Vladislav Lewin <vlewin@suse.de>
2014-08-04 14:26:55 +0200

    strip all recurring whitespaces

--- lib/suse/connect/hwinfo/s390.rb
+++ lib/suse/connect/hwinfo/s390.rb
@@ -22,7 +22,7 @@ class SUSE::Connect::HwInfo::S390 < SUSE::Connect::HwInfo::Base
 
     def hypervisor
       # Strip and remove recurring whitespaces e.g. " z/VM    6.1.0" => "z/VM 6.1.0"
-      output['VM00 Control Program'].strip.sub('   ', '')
+      output['VM00 Control Program'].strip.gsub(/\s+/, ' ')
     end
 
     def uuid
