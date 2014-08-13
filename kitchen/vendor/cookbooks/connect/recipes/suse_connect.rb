ff133512be7d79512a1d19a2743f2257f8e17927
Will Stephenson <wstephenson@suse.de>
2014-08-13 14:56:23 +0200

    Use this branch for testing

--- kitchen/vendor/cookbooks/connect/recipes/suse_connect.rb
+++ kitchen/vendor/cookbooks/connect/recipes/suse_connect.rb
@@ -1,6 +1,6 @@
 git '/tmp/connect' do
   repository 'https://github.com/SUSE/connect.git'
-  reference 'master'
+  reference 'review_140721_handle_zypper_nonzero_returns'
   action 'sync'
   user 'vagrant'
   group 'users'
