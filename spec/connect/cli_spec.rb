a44a8ab4b115c924e5c9efbfbb0aefb313de45e3
Will Stephenson <wstephenson@suse.de>
2014-10-17 13:30:12 +0200

    Add test for url->write_config

--- spec/connect/cli_spec.rb
+++ spec/connect/cli_spec.rb
@@ -120,6 +120,14 @@ describe SUSE::Connect::Cli do
         cli.execute!
       end
 
+      it '--url implies --write-config' do
+        cli = subject.new(%w{-r 123 --url http://foo.test.com})
+        expect(cli.config.write_config).to eq true
+        allow_any_instance_of(SUSE::Connect::Client).to receive(:register!)
+        expect_any_instance_of(SUSE::Connect::Config).to receive(:write!)
+        cli.execute!
+      end
+
     end
 
     context 'status subcommand' do
