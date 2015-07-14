0337249868646b7d19b882a9f3bc26b3e7b5d8b3
Vladislav Lewin <vlewin@suse.de>
2015-07-14 13:33:21 +0200

    add SUSE::Connect::Toolkit::Cast module

---
+++ lib/suse/toolkit/cast.rb
@@ -0,0 +1,28 @@
+module SUSE
+  module Toolkit
+    # Object conversion
+    module Cast
+      def to_openstruct
+        attributes = self.attributes
+        attributes.each do |key,value|
+          attributes[key] = value.map{|v| v.to_openstruct } if value.is_a?(Array) && value.any?{ |v| v.is_a?(Hash) }
+        end
+        OpenStruct.new(attributes)
+      end
+
+      def attributes
+        attributes = self.to_h
+        attributes.each do |k,v|
+          attributes[k] = v.map(&:to_h) if v.is_a?(Array) && v.any?{ |v| v.is_a?(OpenStruct) }
+        end
+        attributes
+      end
+    end
+  end
+end
+
+class Hash
+  def to_openstruct
+    OpenStruct.new self
+  end
+end
