7afa991fc98ee20e5cdaa1c7b99aa220daa54b43
Ladislav Slezák <lslezak@suse.cz>
2015-07-27 09:43:28 +0200

    fixed Rubocop offenses

--- features/step_definition/yast_integration_steps.rb
+++ features/step_definition/yast_integration_steps.rb
@@ -5,34 +5,34 @@ When(/^I call YaST registration "([^"]*)" method(.*)$/) do |method, options|
   yast_registration = Registration::Registration.new(@url)
 
   # pretend YaST is running in the text mode
-  allow(Yast::UI).to receive(:GetDisplayInfo).and_return("TextMode" => true)
+  allow(Yast::UI).to receive(:GetDisplayInfo).and_return('TextMode' => true)
 
   case method
   # registering the system
-  when "register"
+  when 'register'
     base_product = Registration::SwMgmt.find_base_product
-    target_distro = base_product["register_target"]
-    @yast_return = yast_registration.register("email", @regcode, target_distro)
-  when "register_product"
+    target_distro = base_product['register_target']
+    @yast_return = yast_registration.register('email', @regcode, target_distro)
+  when 'register_product'
     case options
     # registering the base product
-    when ""
+    when ''
       base_product_data = Registration::SwMgmt.base_product_to_register
-      base_product_data["reg_code"] = @regcode
+      base_product_data['reg_code'] = @regcode
       allow(Yast::Mode).to receive(:commandline).and_return(true)
-      @yast_return = yast_registration.register_product(base_product_data, "email")
+      @yast_return = yast_registration.register_product(base_product_data, 'email')
     # registering the SDK extension
     when " with \"sdk\" product"
       sdk = {
-        "name"     => "sle-sdk",
-        "arch"     => "x86_64",
-        "version"  => "12"
+        'name'     => 'sle-sdk',
+        'arch'     => 'x86_64',
+        'version'  => '12'
       }
       @yast_return = yast_registration.register_product(sdk)
     end
-  when "get_addon_list"
+  when 'get_addon_list'
     @yast_return = yast_registration.get_addon_list
-  when "migration_products"
+  when 'migration_products'
     installed_products = ::Registration::SwMgmt.installed_products.map do |product|
       ::Registration::SwMgmt.remote_product(product)
     end
@@ -44,15 +44,14 @@ When(/^I call YaST registration "([^"]*)" method(.*)$/) do |method, options|
 end
 
 Then(/^the returned list should contain the "([^"]*)" extension$/) do |name|
-  sdk = @yast_return.find{|ext| ext.identifier == name}
+  sdk = @yast_return.find {|ext| ext.identifier == name }
   expect(sdk).to_not be_nil
 end
 
 Then(/^the result should contain the SP(\d+) migration target$/) do |version|
-  migration = @yast_return.find do |migration|
-    migration.any?{|product| product.version="12.#{version}"}
+  migration_target = @yast_return.find do |migration|
+    migration.any? {|product| product.version = "12.#{version}" }
   end
 
-  expect(migration).to_not be_nil
+  expect(migration_target).to_not be_nil
 end
-
