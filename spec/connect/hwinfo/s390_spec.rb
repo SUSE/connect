ec645f29e6ccb34d077399c499869d34074735bb
Vladislav Lewin <vlewin@suse.de>
2015-09-10 15:31:04 +0200

    refactor and improve tests

--- spec/connect/hwinfo/s390_spec.rb
+++ spec/connect/hwinfo/s390_spec.rb
@@ -3,9 +3,8 @@ require 'suse/connect/hwinfo/s390'
 
 describe SUSE::Connect::HwInfo::S390 do
   subject { SUSE::Connect::HwInfo::S390 }
-  let(:success) { double('Process Status', :exitstatus => 0) }
   let(:read_values) { File.read(File.join(fixtures_dir, 'read_values_s.txt')) }
-  let(:read_values_lpar) { File.read(File.join(fixtures_dir, 'read_values_s_lpar.txt')) }
+  let(:success) { double('Process Status', :exitstatus => 0) }
   include_context 'shared lets'
 
   before :each do
@@ -17,35 +16,6 @@ describe SUSE::Connect::HwInfo::S390 do
   after(:each) do
     SUSE::Connect::HwInfo::Base.instance_variable_set('@arch', nil)
     SUSE::Connect::HwInfo::S390.instance_variable_set('@output', nil)
-  end
-
-  it 'returns a hwinfo hash for s390 systems' do
-    expect(Open3).to receive(:capture3).with(shared_env_hash, 'read_values -u').and_return(['', '', success])
-
-    hwinfo = subject.hwinfo
-    expect(hwinfo[:hostname]).to eq 'test'
-    expect(hwinfo[:cpus]).to eq 1
-    expect(hwinfo[:sockets]).to eq 1
-    expect(hwinfo[:hypervisor]).to eq 'z/VM 6.1.0'
-    expect(hwinfo[:arch]).to eq 's390x'
-    expect(hwinfo[:uuid]).to eq nil
-  end
-
-  it 'returns system cpus count' do
-    expect(subject.cpus).to eql 1
-  end
-
-  it 'returns system sockets count' do
-    expect(subject.sockets).to eql 1
-  end
-
-  it 'returns system hypervisor' do
-    expect(subject.hypervisor).to eql 'z/VM 6.1.0'
-  end
-
-  it 'returns no hypervisor when running on an LPAR' do
-    allow(Open3).to receive(:capture3).with(shared_env_hash, 'read_values -s').and_return([read_values_lpar, '', success])
-    expect(SUSE::Connect::HwInfo::S390.hypervisor).to be nil
   end
 
   describe '#uuid' do
@@ -60,10 +30,59 @@ describe SUSE::Connect::HwInfo::S390 do
     end
   end
 
-  it 'parses output of read_values and returns hash' do
-    expect(subject.send(:output)).to be_kind_of Hash
-    expect(subject.send(:output)).to include 'VM00 CPUs Total'
-    expect(subject.send(:output)).to include 'VM00 IFLs'
-    expect(subject.send(:output)).to include 'VM00 Control Program'
+  context 'Virtual' do
+    it 'parses output of read_values and returns hash' do
+      expect(subject.send(:output)).to be_kind_of Hash
+      expect(subject.send(:output)).to include 'VM00 CPUs Total'
+      expect(subject.send(:output)).to include 'VM00 IFLs'
+      expect(subject.send(:output)).to include 'VM00 Control Program'
+    end
+
+    it 'returns a hwinfo hash for virtual s390 systems' do
+      expect(Open3).to receive(:capture3).with(shared_env_hash, 'read_values -u').and_return(['', '', success])
+
+      hwinfo = subject.hwinfo
+      expect(hwinfo[:hostname]).to eq 'test'
+      expect(hwinfo[:cpus]).to eq 1
+      expect(hwinfo[:sockets]).to eq 1
+      expect(hwinfo[:hypervisor]).to eq 'z/VM 6.1.0'
+      expect(hwinfo[:arch]).to eq 's390x'
+      expect(hwinfo[:uuid]).to eq nil
+    end
+
+    it 'returns system hypervisor' do
+      expect(subject.hypervisor).to eql 'z/VM 6.1.0'
+    end
   end
+
+  context 'Physical' do
+    let(:read_values) { File.read(File.join(fixtures_dir, 'read_values_s_lpar.txt')) }
+
+    before :each do
+      allow(Open3).to receive(:capture3).with(shared_env_hash, 'read_values -s').and_return([read_values, '', success])
+      allow(Open3).to receive(:capture3).with(shared_env_hash, 'read_values -u').and_return(['', '', success])
+    end
+
+    it 'parses output of read_values and returns hash' do
+      expect(subject.send(:output)).to be_kind_of Hash
+      expect(subject.send(:output)).not_to include 'VM00 CPUs Total'
+      expect(subject.send(:output)).not_to include 'VM00 IFLs'
+      expect(subject.send(:output)).not_to include 'VM00 Control Program'
+    end
+
+    it 'returns a hwinfo hash for physical s390 systems' do
+      hwinfo = subject.hwinfo
+      expect(hwinfo[:hostname]).to eq 'test'
+      expect(hwinfo[:cpus]).to eq 2
+      expect(hwinfo[:sockets]).to eq 2
+      expect(hwinfo[:hypervisor]).to eq nil
+      expect(hwinfo[:arch]).to eq 's390x'
+      expect(hwinfo[:uuid]).to eq nil
+    end
+
+    it 'returns no hypervisor when running on an LPAR' do
+      expect(SUSE::Connect::HwInfo::S390.hypervisor).to be nil
+    end
+  end
+
 end
