c0e21e1e25234e1887a4dec8db759d9280b65cd2
Thomas Schmidt <tschmidt@suse.de>
2016-01-25 16:09:09 +0100

    use --match-exact (bnc#952804)

--- lib/suse/connect/zypper.rb
+++ lib/suse/connect/zypper.rb
@@ -81,7 +81,7 @@ module SUSE
         # @param product identifier [String]
         # Returns an array of hashes of all solvable products
         def find_products(identifier)
-          zypper_out = call("--xmlout --no-refresh --non-interactive search -s -t product #{Shellwords.escape(identifier)}", false)
+          zypper_out = call("--xmlout --no-refresh --non-interactive search --match-exact -s -t product #{Shellwords.escape(identifier)}", false)
           xml_doc = REXML::Document.new(zypper_out, compress_whitespace: [])
           xml_doc.elements.to_a('stream/search-result/solvable-list/solvable').map(&:to_hash)
         end
