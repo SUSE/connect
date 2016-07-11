1eee711ffb33f7df285da941039d7f229da4599c
Thomas Schmidt <tschmidt@suse.de>
2016-07-11 17:01:37 +0200

    just pull the logging of stderr out of the if block

--- lib/suse/toolkit/system_calls.rb
+++ lib/suse/toolkit/system_calls.rb
@@ -8,13 +8,13 @@ module SUSE
 
       def execute(cmd, quiet = true, valid_exit_codes = [0]) # rubocop:disable CyclomaticComplexity
         log.debug("Executing: '#{cmd}' Quiet: #{quiet}")
-        output, error, status = Open3.capture3({ 'LC_ALL' => 'C' }, cmd) {|_stdin, stdout, stderr, _wait_thr| stderr.read  + stdout.read }
+        output, error, status = Open3.capture3({ 'LC_ALL' => 'C' }, cmd) {|_stdin, stdout, _stderr, _wait_thr| stdout.read }
         log.debug("Output: '#{output.strip}'") unless output.empty?
+        log.debug("Error: '#{error.strip}'") unless error.empty?
 
         # Catching interactive failures of zypper. --non-interactive always returns with exit code 0 here
         if !valid_exit_codes.include?(status.exitstatus) || error.include?('ABORT request')
           log.error("command '#{cmd}' failed")
-          log.debug("Error: '#{error.strip}'") unless error.empty?
           # NOTE: zypper with formatter option will return output instead of error
           # e.g. command 'zypper --xmlout --non-interactive products -i' failed
           error = error.empty? ? output.strip : error.strip
