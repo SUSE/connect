bd0b9e6064ca798b1ff85ba26a3236776a6b7a69
Will Stephenson <wstephenson@suse.de>
2016-09-19 10:34:15 +0200

    Support integration testing on different products

--- docker/integration.sh
+++ docker/integration.sh
@@ -1,4 +1,10 @@
 #!/bin/sh -xe
-rake build &&
-zypper --non-interactive --no-gpg-checks in /var/tmp/build-root/SLE_12-x86_64/home/abuild/rpmbuild/RPMS/x86_64/* &&
+if [ -z $PRODUCT ]
+then
+  echo "PRODUCT env var for integration testing not set!"
+  exit 1
+fi
+
+rake build[$PRODUCT] &&
+zypper --non-interactive --no-gpg-checks in /var/tmp/build-root/$PRODUCT-x86_64/home/abuild/rpmbuild/RPMS/x86_64/* &&
 cucumber /tmp/connect/features/activation.feature /tmp/connect/features/activation_errors.feature
