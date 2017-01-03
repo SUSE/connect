98166736ae8a3c5a4c50fbc0361fd9fac4d814db
Kirill Pimenov <kpimenov@suse.de>
2017-01-03 14:41:34 +0100

    RSpec instead of Aruba

--- features/step_definition/library_steps.rb
+++ features/step_definition/library_steps.rb
@@ -1,7 +1,7 @@
 When(/^I should receive '(.*)' as a migration target$/) do |target_name|
   products = SUSE::Connect::Status.new(@client.config).system_products.map(&:to_openstruct)
-  migrations =  @client.system_migrations(products).to_s
-  assert_partial_output(target_name, migrations)
+  migration_targets  =  @client.system_migrations(products).flatten.uniq.map(&:shortname)
+  expect(migration_targets).to include(target_name)
 end
 
 When(/^I call the migration rollback method$/) do
