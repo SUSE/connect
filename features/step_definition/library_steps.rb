837b44c2f6f205a520aa195ee49fdfb12127dc38
Kirill Pimenov <kpimenov@suse.de>
2017-01-03 14:25:55 +0100

    More robust migration targets matching

--- features/step_definition/library_steps.rb
+++ features/step_definition/library_steps.rb
@@ -1,7 +1,7 @@
 When(/^I should receive '(.*)' as a migration target$/) do |target_name|
   products = SUSE::Connect::Status.new(@client.config).system_products.map(&:to_openstruct)
-  migrations =  @client.system_migrations(products)
-  assert_exact_output(target_name, migrations.first.first.shortname)
+  migrations =  @client.system_migrations(products).to_s
+  assert_partial_output(target_name, migrations)
 end
 
 When(/^I call the migration rollback method$/) do
