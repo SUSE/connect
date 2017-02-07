41b65dfadcbb46386b34778f08923a7d85ffb1e0
Thomas Hutterer <thutterer@suse.de>
2017-02-07 16:56:35 +0100

    rubocop fix

--- spec/connect/connection_spec.rb
+++ spec/connect/connection_spec.rb
@@ -132,7 +132,8 @@ describe SUSE::Connect::Connection do
 
     it 'provides a user-friendly error message' do
       expect(connection.http).to receive(:request).and_raise(Zlib::BufError)
-      expect { connection.send(:json_request, :get, '/api/v1/megusta') }.to raise_error(SUSE::Connect::NetworkError, 'Network error. Try again and report a bug if it keeps happening.')
+      expect { connection.send(:json_request, :get, '/api/v1/megusta') }.to \
+        raise_error(SUSE::Connect::NetworkError, 'Network error. Try again and report a bug if it keeps happening.')
     end
   end
 
