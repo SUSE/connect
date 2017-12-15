dafcecf19cd2660ce7f6ade863598f53bfd95e84
Hernan Schmidt <lagartoflojo@users.noreply.github.com>
2017-12-15 16:40:58 +0100

    Fix feature spec with new method signature

--- features/step_definition/library_steps.rb
+++ features/step_definition/library_steps.rb
@@ -4,7 +4,7 @@ end
 
 When(/^I should receive '(.*)' as a migration target$/) do |target_name|
   products = SUSE::Connect::Status.new(@client.config).system_products.map(&:to_openstruct)
-  migration_targets  =  @client.system_migrations(products).flatten.uniq.map(&:shortname)
+  migration_targets = @client.system_migrations(products, kind: :online).flatten.uniq.map(&:shortname)
   expect(migration_targets).to include(target_name)
 end
 
