631c6bb3745a5b68e89e816905ce67b71b5df981
Hernan Schmidt <lagartoflojo@users.noreply.github.com>
2018-02-28 17:01:37 +0100

    Properly test connect cli exit codes
    `Kernel.exit` was getting stubbed, but our code assumed that after
    calling `exit`, no more code would get run. Stubbing `exit` breaks that
    expectation, since the code keeps running.
    Also, we were not testing the error codes we were throwing in different
    scenarios. We are now.

---
+++ spec/support/matchers/exit_with_code.rb
@@ -0,0 +1,28 @@
+# From: https://stackoverflow.com/a/44275447
+# Usage:
+# it 'exits with code 42' do
+#   expect { exit 42 }.to exit_with_code(42)
+# end
+
+RSpec::Matchers.define :exit_with_code do |exp_code|
+  actual = nil
+  match do |block|
+    begin
+      block.call
+    rescue SystemExit => e
+      actual = e.status
+    end
+    actual and actual == exp_code
+  end
+  failure_message do |block|
+    "expected block to call exit(#{exp_code}) but exit" +
+        (actual.nil? ? " not called" : "(#{actual}) was called")
+  end
+  failure_message_when_negated do |block|
+    "expected block not to call exit(#{exp_code})"
+  end
+  description do
+    "expect block to call exit(#{exp_code})"
+  end
+  supports_block_expectations
+end
