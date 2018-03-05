4a4fece8609874146041287b46d5e3d17cd8012f
Hernan Schmidt <lagartoflojo@users.noreply.github.com>
2018-03-05 10:08:15 +0100

    Load OS env vars from YAML files
    Some integration tests need different data depending on the base OS we
    are running. By having this YAML file, we can encapsulate all of those
    differences in a single place.

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,6 +4,8 @@ require 'suse/connect'
 require 'aruba/cucumber'
 require 'cucumber/rspec/doubles'
 
+OPTIONS = YAML.load_file(File.join(__dir__, 'environments.yml')).fetch(ENV.fetch('PRODUCT'))
+
 Before('@slow_process') do
   aruba.config.io_wait_timeout = 90
   aruba.config.exit_timeout = 90
