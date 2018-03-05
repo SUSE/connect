45aac0e4429177f481acae874524294e1a523eaf
Hernan Schmidt <lagartoflojo@users.noreply.github.com>
2018-03-05 10:05:09 +0100

    Remove ENV['PRODUCT'] dance
    This env var is always there now

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,13 +10,9 @@ Before('@slow_process') do
 end
 
 Before('@libzypplocked') do
-  # this should put the pid of the cucumber process into the lockfile
-  @old_product_env_var_contents = ENV['PRODUCT']
-  ENV['PRODUCT'] = SUSE::Connect::Zypper.base_product.identifier + '_' + SUSE::Connect::Zypper.base_product.version
   `echo $PPID > /var/run/zypp.pid`
 end
 
 After('@libzypplocked') do
   `rm /var/run/zypp.pid`
-  ENV['PRODUCT'] = @old_product_env_var_contents
 end
