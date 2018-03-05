3f0c648a257afb832410cd78b8742eb02a554727
Hernan Schmidt <lagartoflojo@users.noreply.github.com>
2018-03-05 17:20:51 +0100

    Run all cucumber tests in integration.sh

--- docker/integration.sh
+++ docker/integration.sh
@@ -10,8 +10,4 @@ rake package:build_gem &&
 rake package:generate_manpages &&
 cd package && osc build $PRODUCT x86_64 --no-verify --trust-all-projects && cd .. &&
 zypper --non-interactive --no-gpg-checks in /oscbuild/$PRODUCT-x86_64/home/abuild/rpmbuild/RPMS/x86_64/* &&
-cucumber /tmp/connect/features/activation.feature \
-         /tmp/connect/features/activation_errors.feature \
-         /tmp/connect/features/help.feature \
-         /tmp/connect/features/version.feature \
-         /tmp/connect/features/localization.feature
+cucumber
