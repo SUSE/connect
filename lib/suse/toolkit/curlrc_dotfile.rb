d2fc801152b2a6d30596d397df5a22127aa2dd57
Thomas Schmidt <tschmidt@suse.de>
2020-10-02 17:40:14 +0200

    Make it more clear in the comment where Yast stores the proxy config values

--- lib/suse/toolkit/curlrc_dotfile.rb
+++ lib/suse/toolkit/curlrc_dotfile.rb
@@ -1,11 +1,11 @@
 require 'etc'
 
-# When configuring a proxy with "yast2 proxy", the proxy url and credentials
-# are written to ~/.curlrc and /etc/sysconfig/proxy
-# At login session init, the values get copied to the environment from /etc/sysconfig/proxy,
+# When configuring a proxy with "yast2 proxy", the proxy url is written to ~/.curlrc
+# and /etc/sysconfig/proxy. The proxy credentials are only written to ~/.curlrc.
+# At login session init, the values from /etc/sysconfig/proxy are set in the environment,
 # from where Net::Http is picking them up. Unfortunately the proxy credentials are
-# not prepended to the url, but stored seperately in a way that Net::Http isn't picking
-# them up. That's why we need to parse them from .curlrc
+# not prepended to the proxy url, but stored seperately in .curlrc. Net::Http isn't picking
+# them up automatically from there, that's why we need to parse them manually from .curlrc
 
 class SUSE::Toolkit::CurlrcDotfile
   CURLRC_LOCATION = '.curlrc'
